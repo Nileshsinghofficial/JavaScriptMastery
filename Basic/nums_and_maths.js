741d87a960a8400bbdf05ddb9ef8738c433a72b4
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-08 20:15:10 +0530

    number and math function

---
+++ Basic/nums_and_maths.js
@@ -0,0 +1,22 @@
+ const balance = new Number(100)
+//  console.log(balance.toFixed(2));  // fixed decimal to digits
+//  console.log(balance.toString().length);// Change into string
+
+ const newNumber = 123.877
+//  console.log(newNumber.toPrecision(4)) // roud off at four position value
+
+ const hundreds = 1000000
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+
+//  ++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math); 
+// console.log(Math.abs(-4)); // Change into postive in value
+// console.log(Math.round(4.49)); // change in round off 
+// console.log(Math.ceil(44.3)); // ceiling value like 44.3 => 45
+// console.log(Math.floor(44.2)); // floar value like 44.3 => 44
+// console.log(Math.min(3,5,6,4,3)); // Select minumum value
+// console.log(Math.max(7,4,5,2,3)); // Select Maximum value
+
+console.log((Math.floor(Math.random()*10)) + 1);
