20eef25bbfc304e9d0c8ba5fc086b752a8a3093c
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-08 20:16:13 +0530

    comparision operator

---
+++ Basic/comprision.js
@@ -0,0 +1,12 @@
+// console.log("2" == 2) // true
+// console.log(null > 0) //false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+
+// console.log(undefined == 0) // false
+// console.log(undefined == null) // true
+// console.log(undefined != 0 ) // true
+// console.log(undefined < 0) // false
+// console.log(undefined < null) // false 
+
+console.log("2" === 2);
