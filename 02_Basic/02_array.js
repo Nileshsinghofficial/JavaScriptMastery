ea29866c16d698fe89662d4321243434f0071b90
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-09 17:58:12 +0530

    concatnets arrays

--- 02_Basic/02_array.js
+++ 02_Basic/02_array.js
@@ -13,5 +13,11 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros.flat());
 
 /* Spread method to concate array  */
-const all_new_heros = [...marvel_heros,...dc_heros]
-console.log(all_new_heros);
+const all_new_heros = [...marvel_heros,...dc_heros] // same just like concatnate
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7,[4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array); // array inside array change into single array
+
