52c02a7f9ea97ae94b1e479c6310411264cf6512
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-09 18:14:37 +0530

    of use into array convert array, variable into array

--- 02_Basic/02_array.js
+++ 02_Basic/02_array.js
@@ -29,4 +29,4 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3))
+// console.log(Array.of(score1, score2, score3)) // use of convert(variable, array) into array 
