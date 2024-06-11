876fe508947883372f0593b4f7f990665786ff67
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-11 11:12:28 +0530

    how to give default user name if user not logged in

--- 03_Basic/01_function.js
+++ 03_Basic/01_function.js
@@ -11,9 +11,33 @@ function sayMyName(){
 
 // sayMyName(); 
 
+// function addTwoNumber(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// const result = addTwoNumber(3,4)
+// console.log("Result:", result)
+
+
+// How to get Return in function
+
 function addTwoNumber(number1, number2){
-    console.log(number1 + number2)
+    // let result =  number1 + number2
+    // return result  
+    return number1 + number2
 }
 
-const result = addTwoNumber(3,4)
-console.log("Result:", result)
+const result = addTwoNumber(3, 5)
+// console.log("Result: ", result)
+
+function loginUserMessage(username = "John"){  // give a default user name if not give a user name in function
+    // if(username === undefined){
+    if(!username){
+        console.log("Please Enter a Username");
+        return
+    }
+    return `${username} Just logged In `
+}
+
+// console.log(loginUserMessage("Nilesh"))
+console.log(loginUserMessage());
