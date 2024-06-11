708013278eae581096c6185b1014df2eb871c122
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-11 10:06:07 +0530

    function

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,12 @@
+
+
+function sayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("L");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+sayMyName(); 
