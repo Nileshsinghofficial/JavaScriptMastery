4268a2730611318bc0bfb85938a62b73a3fbf26a
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-14 18:36:40 +0530

    arrow basic completed

--- 03_Basic/03_arrow.js
+++ 03_Basic/03_arrow.js
@@ -22,10 +22,40 @@ const user = {
 
 // Outpur => Undefined
 
-const chai = function(){
-    let username = "nilesh"
-    console.log(this.username); // this function not print username here he work in object as well.
-}
+// const chai = function(){
+//     let username = "nilesh"
+//     console.log(this.username); // this function not print username here he work in object as well.
+// }
 
 // output => undefined
-chai()
+
+/* Arrow Function */
+
+// const chai = () =>  {
+//     let username = "nilesh"
+//     console.log(this.username);
+// }
+
+// // chai()
+
+/* Basic Arrow Function */
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+/* Implict Return Array Function */
+// const addTwo = (num1, num2) => num1 + num2 //in one line print . remove return function
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// *Note => if we use parenthasis ( ) then should be use return keywords.
+
+// in object we must use ( ) and delcere object inside.
+
+const addTwo = (num1, num2) => ({username:"Nilesh Singh"})
+
+console.log(addTwo(2,3)); 
+
+// arrrow function 
+
+const myArray = [2, 3, 4, 5, 6, 7]
+myArray.forEach(()=> {} )
