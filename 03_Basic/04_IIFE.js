4aeef0bea8ecc5bd6e2ed5c732fd28a56419bfce
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-15 22:11:04 +0530

    Imeddite invoked function express

--- 03_Basic/04_IIFE.js
+++ 03_Basic/04_IIFE.js
@@ -1,1 +1,10 @@
 // Immediately Invoked Function Expressions (IIFE)
+(function chai(){
+    // name IIFE 
+    console.log(`DB CONNECTED`);
+}) ();  // use ; s  emi-colon stop this function
+
+( (name) => {
+    console.log(`DB Connected ${name}`);
+} )('Nilesh');
+
