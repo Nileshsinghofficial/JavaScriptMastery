3229c26b8b3b2635fa3ceb65c6fddda34c39b513
nileshsinghofficial <nigamsingh888@gmail.com>
2024-06-15 22:11:48 +0530

    if else

---
+++ 04_control_Flow/one.js
@@ -0,0 +1,64 @@
+// if
+const isUserloggedIn = true
+
+// if ( 3 != 3){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+const temperature = 41
+
+if(temperature === 41){
+    // console.log("less then 50");
+}
+else {
+    // console.log("temperature is greater than 50");
+}
+
+// >, <, greater then and less then
+// Comprision Operatior
+// == comprision operator
+// = operator Assignment operator
+// != not equal to
+// === check datatypes also/
+
+const score = 200
+if(score > 100){
+    let power = "fly"
+    // console.log(`User power: ${power}`);
+}
+
+// console.log(`User power: ${power}`);
+
+const balance = 749
+
+// if(balance > 500){
+//     console.log("test");
+//     console.log("test2");
+// }
+
+// if (balance < 500){
+//     console.log("Less then");
+// } else if(balance < 750){
+//     console.log("less then 750");
+// } else if(balance < 900){
+//     console.log("balance is less then 900");
+// }
+// else{
+//     console.log("Balance is Greater then 1000");
+// }
+
+const UserloggedIn = true
+const debitCard = true
+if (UserloggedIn && debitCard){
+    // console.log("Allow to Bye Course");
+}
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User LoggedIn");
+}
